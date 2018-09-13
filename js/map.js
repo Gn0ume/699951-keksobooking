6aef5eafbd390fc2543d166b685d68e982292b38
AntonTimichev <enfild2008@yandex.ru>
2018-09-13 20:41:31 +0300

    активация страницы, просмотр объявлений

--- js/map.js
+++ js/map.js
@@ -1,50 +1,11 @@
 'use strict';
 
-function getRandomInteger(min, max) {
-  var randomNumber = min + Math.random() * (max + 1 - min);
-  return Math.floor(randomNumber);
-}
-
-function getRandomArrayElement(arr) {
-  var min = 0;
-  var max = arr.length - 1;
-  var randomElement = getRandomInteger(min, max);
-
-  return arr[randomElement];
-}
-
-function getMixArray(arr) {
-  var j;
-  var temp;
-  for (var i = arr.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = arr[j];
-    arr[j] = arr[i];
-    arr[i] = temp;
-  }
-  return arr;
-}
-
-function getArrRandomLength(arr) {
-  var min = 0;
-  var max = arr.length;
-  var randomIndex = getRandomInteger(min, max);
-
-  return arr.slice(0, randomIndex);
-}
-
+var amountCards = 8;
 var photos = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-
-function getPhotos(array) {
-  var arrPhotos = array.slice();
-  return getMixArray(arrPhotos);
-}
-
-var amountCards = 8;
 var titles = [
   'Большая уютная квартира',
   'Маленькая неуютная квартира',
@@ -65,15 +26,55 @@ var features = [
   'elevator',
   'conditioner'
 ];
+var map = document.querySelector('.map');
+var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
+var templateCard = document.querySelector('#card').content.querySelector('.map__card');
+var offers = getOffers();
+
+function getRandomInteger(min, max) {
+  var randomNumber = min + Math.random() * (max + 1 - min);
+  return Math.floor(randomNumber);
+}
+
+function getRandomArrayElement(arr) {
+  var min = 0;
+  var max = arr.length - 1;
+  var randomElement = getRandomInteger(min, max);
+  return arr[randomElement];
+}
+
+function getMixArray(arr) {
+  var j;
+  var temp;
+  for (var i = arr.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[j];
+    arr[j] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
+
+function getArrRandomLength(arr) {
+  var min = 0;
+  var max = arr.length;
+  var randomIndex = getRandomInteger(min, max);
+  return arr.slice(0, randomIndex);
+}
+
+function getPhotos(array) {
+  var arrPhotos = array.slice();
+  return getMixArray(arrPhotos);
+}
 
 function getOffers() {
-
   var arrOffers = [];
   var titlesMix = getMixArray(titles);
   for (var i = 0; i < amountCards; i++) {
     var locationX = getRandomInteger(1, 1050);
     var locationY = getRandomInteger(130, 630);
     var card = {
+      id: i,
       author: {
         avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
@@ -100,21 +101,16 @@ function getOffers() {
   return arrOffers;
 }
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
 /*
 СОЗДАНИЕ ПИНА
 */
-var offers = getOffers();
-var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
-
 function renderPin(obj) {
   var pinElement = templatePin.cloneNode(true);
+  pinElement.dataset.id = obj.id;
   pinElement.style.top = obj.location.y + 'px';
   pinElement.style.left = obj.location.x + 'px';
   pinElement.querySelector('img').src = obj.author.avatar;
   pinElement.querySelector('img').alt = obj.offer.title;
-
   return pinElement;
 }
 
@@ -133,13 +129,11 @@ function addPins(array) {
   return mapPins.appendChild(newPin);
 }
 
-addPins(offers);
 /*
 *СОЗДАНИЕ ОБЪЯВЛЕНИЯ
 */
 function getFragmentFeatures(array) {
   var fragmentFeatures = document.createDocumentFragment();
-
   for (var i = 0; i < array.length; i++) {
     var newElement = document.createElement('li');
     var className = 'popup__feature--' + array[i];
@@ -198,10 +192,12 @@ function getTextCapacity(obj) {
   return textCapacity;
 }
 
-var templateCard = document.querySelector('#card').content.querySelector('.map__card');
-
 function createNoticeElement(obj) {
   var cardElement = templateCard.cloneNode(true);
+  var cardFeatures = cardElement.querySelector('.popup__features');
+  var featuresFragment = getFragmentFeatures(obj.offer.features);
+  var cardPhotos = cardElement.querySelector('.popup__photos');
+  var photosFragment = getFragmentPhotos(obj.offer.photos);
   cardElement.querySelector('.popup__title').textContent = obj.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = obj.offer.address;
   cardElement.querySelector('.popup__text--price').innerHTML = obj.offer.price + '&#x20bd<span>/ночь</span>';
@@ -210,23 +206,111 @@ function createNoticeElement(obj) {
   cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + obj.offer.checkin + ' выезд после ' + obj.offer.checkout;
   cardElement.querySelector('.popup__description').textContent = obj.offer.description;
   cardElement.querySelector('.popup__avatar').src = obj.author.avatar;
-  var cardFeatures = cardElement.querySelector('.popup__features');
-  var featuresFragment = getFragmentFeatures(obj.offer.features);
   cardFeatures.innerHTML = '';
   cardFeatures.appendChild(featuresFragment);
-  var cardPhotos = cardElement.querySelector('.popup__photos');
-  var photosFragment = getFragmentPhotos(obj.offer.photos);
   cardPhotos.innerHTML = '';
   cardPhotos.appendChild(photosFragment);
-
   return cardElement;
 }
 
 function addNotice(obj) {
   var newNotice = createNoticeElement(obj);
   map.appendChild(newNotice);
+  var popupClose = map.querySelector('.popup__close');
+  popupClose.addEventListener('click', onClickCloseOffer);
+  document.addEventListener('keydown', onOfferEscPress);
 }
 
-addNotice(offers[0]);
+/*
+MODULE4 - TASK1
+*/
+var HIEGHT_PIN = 20;
+var ESC_KEYCODE = 27;
+var mapPinMain = map.querySelector('.map__pin--main');
+var adForm = document.querySelector('.ad-form');
+var adFormFieldSets = adForm.querySelectorAll('fieldset');
+var mapFilters = document.querySelector('.map__filters');
+var filterFormItems = mapFilters.querySelectorAll('.map__filters > *');
+var address = adForm.querySelector('#address');
+var Coords = getAddress(mapPinMain);
+
+changeFieldFilterForm(filterFormItems);
+changeFieldAdForm(adFormFieldSets);
+setAddress(Coords);
+
+map.addEventListener('click', onClickPin);
+mapPinMain.addEventListener('mouseup', onClickMainPin);
+
+function onClickPin(evt) {
+  var pin = evt.target.closest('.map__pin:not(.map__pin--main)');
+  var mapCard = map.querySelector('.map__card');
+  if (pin) {
+    var id = pin.dataset.id;
+    var data = offers[id];
+    if (mapCard) {
+      map.removeChild(mapCard);
+    }
+    addNotice(data);
+  }
+}
+
+function closeOffer() {
+  var mapCard = map.querySelector('.map__card');
+  map.removeChild(mapCard);
+  document.removeEventListener('keydown', onOfferEscPress);
+}
+
+function onClickCloseOffer() {
+  closeOffer();
+}
+
+function onOfferEscPress(evt) {
+  if (evt.which === ESC_KEYCODE) {
+    closeOffer();
+  }
+}
+
+function changeFieldAdForm(array) {
+  var isFormDisabled = map.classList.contains('map--faded');
+  for (var i = 0; i < array.length; i++) {
+    array[i].disabled = isFormDisabled;
+  }
+}
+
+function changeFieldFilterForm(array) {
+  var isFormDisabled = map.classList.contains('map--faded');
+  for (var i = 0; i < array.length; i++) {
+    array[i].disabled = isFormDisabled;
+  }
+}
+
+function activatePage() {
+  map.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+}
+
+function onClickMainPin() {
+  activatePage();
+  addPins(offers);
+  changeFieldAdForm(adFormFieldSets);
+  changeFieldFilterForm(filterFormItems);
+  var addressCoords = getAddress(mapPinMain);
+  setAddress(addressCoords);
+}
+
+function setAddress(coords) {
+  address.value = coords.x + ', ' + coords.y;
+}
+
+function getAddress(elem) {
+  var pinLeft = elem.style.left;
+  var pinTop = elem.style.top;
+  var x = parseInt(pinLeft, 10) + Math.round(elem.clientWidth / 2);
+  var y = parseInt(pinTop, 10) + Math.round(elem.clientHeight + HIEGHT_PIN);
+  if (map.classList.contains('map--faded')) {
+    y = parseInt(pinTop, 10) + Math.round(elem.clientHeight / 2);
+  }
+  return {x: x, y: y};
+}
 
 
